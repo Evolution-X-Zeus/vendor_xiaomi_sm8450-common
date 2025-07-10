89ab8189bbfbf5710582292bc374ff0f631839a9
Joey <joey@evolution-x.org>
2025-07-10 10:36:22 +0200

    sm8450-common: Import missing blobs from diting OS2.0.3.0.VLFMIXM

--- sm8450-common-vendor.mk
+++ sm8450-common-vendor.mk
@@ -256,10 +256,12 @@ PRODUCT_PACKAGES += \
     libVkLayer_ADRENO_qprofiler \
     libq3dtools_adreno \
     libq3dtools_esx \
+    libC2D2 \
     libCB \
     libOpenCL \
     libadreno_app_profiles \
     libadreno_utils \
+    libc2d30_bltlib \
     libdolbyottcameracontrol \
     libdolbyvision \
     libgpudataproducer \
@@ -540,6 +542,7 @@ PRODUCT_PACKAGES += \
     libsnsapi \
     libsnsdiaglog \
     libsoc_helper \
+    libsoc_helper_jni \
     libspcom \
     libspl \
     libssc \
@@ -799,6 +802,7 @@ PRODUCT_PACKAGES += \
     IWlanService \
     SoterService \
     TimeService \
+    TrustZoneAccessService \
     com.qualcomm.qti.gpudrivers.taro.api31 \
     uimgbaservice \
     HotwordEnrollmentXGoogleHEXAGON_WIDEBAND \
